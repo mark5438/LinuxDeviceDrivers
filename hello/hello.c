eedab730f47b8b03dab83e793604115fb985895d
Markus Rytter <markus.r@live.dk>
2022-10-26 09:03:20 +0100

    Wrote simple hello world kernel module

---
+++ hello/hello.c
@@ -0,0 +1,21 @@
+#define MODULE
+#define LINUX
+#define __KERNEL__
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+
+static int __init module_start(void) {
+    printk(KERN_INFO " Loaded hello module\n");
+    return 0;
+}
+
+static void __exit module_end(void) {
+    printk(KERN_INFO " Unloaded hello module\n");
+}
+
+module_init(module_start)
+module_exit(module_end)
+
+MODULE_LICENSE("GPL");
